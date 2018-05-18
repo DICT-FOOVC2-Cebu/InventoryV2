7249ad22076f6badf14ee9a4f6823e7de4f379d6
Culoot <38481976+Culoot@users.noreply.github.com>
2018-05-18 13:21:24 +1200

    Update
    with QR Code Generator

--- src/inventorysystemv2/GenerateUIController.java
+++ src/inventorysystemv2/GenerateUIController.java
@@ -5,9 +5,22 @@
  */
 package inventorysystemv2;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -19,9 +32,52 @@ public class GenerateUIController implements Initializable {
     /**
      * Initializes the controller class.
      */
-    @Override
+    @FXML
+    private ImageView ViewImage;
+    
+    
+    
+        @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        String myWeb = "bwesit";
+        int width = 300;
+        int height = 300;
+        String fileType = "png";
+        
+        BufferedImage bufferedImage = null;
+        try {
+            BitMatrix byteMatrix = qrCodeWriter.encode(myWeb, BarcodeFormat.QR_CODE, width, height);
+            bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            bufferedImage.createGraphics();
+            
+            Graphics2D graphics = (Graphics2D) bufferedImage.getGraphics();
+            graphics.setColor(Color.WHITE);
+            graphics.fillRect(0, 0, width, height);
+            graphics.setColor(Color.BLACK);
+            
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    if (byteMatrix.get(i, j)) {
+                        graphics.fillRect(i, j, 1, 1);
+                    }
+                }
+            }
+            
+            System.out.println("Success...");
+            
+        } catch (WriterException ex) {
+            Logger.getLogger(GenerateUIController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        
+        
+        ViewImage.setImage(SwingFXUtils.toFXImage(bufferedImage, null));
+   
+
+    
     }    
+    
     
 }
